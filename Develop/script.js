f52370961e33f60f6387a69cfbba92123437b2fb
josh Watson <joshjkwatson@gmail.com>
2023-03-07 18:38:30 +1100

    first JScode input

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length = number (prompt("Click button for a random password"));
+var charType = promt ("")
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var charSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: '!@#$%^&*()~`:;",<.>/?'
+  };
+  var charSet = charSets[charType.toLowerCase()] || charSets.lowercase
+  var retVal = ""
+  for (var i = 0; i < length; i++)
+  
 
   passwordText.value = password;
 
