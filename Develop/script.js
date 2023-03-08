2481b8e313a284f68530532e364a41487c2bbdec
Joshua Watson <joshjkwatson@gmail.com>
2023-03-08 22:04:48 +1100

    updated JS code

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = ""
+var charToUse = [];
 
+
+function getRandomChar(arr){
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
 
 function generatePassword(){
-  var length = 8 < 128,
-    charSet = lowercase, uppercase, numeric, special,
-    retVal = "";
-  for (var i = 0, n = charSet.length; i < length; ++i){
-    retVal += charSet.charAt(Math.floor(Math.random() * n));
+  
+  var charSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: '!@#$%^&*()~`:;\'",<.>/?' 
+  };
+
+var charCount = parseInt(window.prompt("How many characters would you like in your password"))
+  if (isNaN(charCount)) {
+    window.alert("Please enter a number between 8 and 128")
+    return
   }
-  return retVal;
+if (charCount < 8 || charCount > 128){
+  window.alert("Has to be between 8 and 128 characters")
+}
+
+var useLowercase = window.confirm("Should your password contain lowercase characters")
+if (useLowercase){
+  password += getRandomChar(charSets.lowercase);
+  charToUse.push (...charSets.lowercase)
+}
+
+
+  while(password.length < charCount){
+    password += getRandomChar(charToUse)
+  }
+
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var charSets = {
-    lowercase: 'abcdefghijklmnopqrstuvwxyz',
-    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    numeric: '0123456789',
-    special: '!@#$%^&*()~`:;",<.>/?'
-  };
-  var charSet = charSets[charType.toLowerCase()] || charSets.lowercase
-  var retVal = ""
-  for (var i = 0; i < length; i++)
   
 
   passwordText.value = password;
