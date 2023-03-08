2dbd60061caab126eec4b0b81bdc42bd3b1c8de8
josh Watson <joshjkwatson@gmail.com>
2023-03-08 23:12:36 +1100

    updated JS code

--- Develop/script.js
+++ Develop/script.js
@@ -18,27 +18,50 @@ function generatePassword(){
     special: '!@#$%^&*()~`:;\'",<.>/?' 
   };
 
-var charCount = parseInt(window.prompt("How many characters would you like in your password"))
+var charCount = parseInt(window.prompt("How many characters would you like in your password? Choose between 8 and 128"))
   if (isNaN(charCount)) {
     window.alert("Please enter a number between 8 and 128")
-    return
-  }
-if (charCount < 8 || charCount > 128){
+     }
+     if (charCount < 8 || charCount > 128){
   window.alert("Has to be between 8 and 128 characters")
-}
+  }
 
-var useLowercase = window.confirm("Should your password contain lowercase characters")
+
+var useLowercase = window.confirm("Should your password contain lowercase characters?")
 if (useLowercase){
   password += getRandomChar(charSets.lowercase);
   charToUse.push (...charSets.lowercase)
 }
 
+var useUppercase = window.confirm("Do you want your password contain uppercase characters?")
+if (useUppercase){
+  password += getRandomChar(charSets.uppercase);
+  charToUse.push (...charSets.uppercase);
+}
+
+var useNumeric = window.confirm("Do you want your password to contain numbers?")
+if (useNumeric){
+  password += getRandomChar(charSets.numeric);
+  charToUse.push (...charSets.numeric)
+}
+
+var useSpecial = window.confirm("Do you want your password to contain special characters?")
+if(useSpecial){
+  window.confirm = false
+}
+else if (useSpecial){ 
+  password += getRandomChar(charSets.special);
+  charToUse.push (...charSets.special)
+}
 
   while(password.length < charCount){
     password += getRandomChar(charToUse)
   }
-
-  return password;
+if (!useLowercase && !useNumeric && !useSpecial && !useUppercase){
+  choices = alert("You must choose at least one")
+  
+}
+  
 }
 // Write password to the #password input
 function writePassword() {
