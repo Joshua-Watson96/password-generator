04c7a89efe1d35ed9f8a65c335c6e8ff17dd3c3a
josh Watson <joshjkwatson@gmail.com>
2023-03-09 21:25:53 +1100

    revamped code

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,93 @@
 // Assignment Code
+// Global variables used.
 var generateBtn = document.querySelector("#generate");
-var password = ""
-var charToUse = [];
+var password = [];
+var charLowercase;
+var charUppercase;
+var charNumber;
+var charSpecial;
+var charCount;
+var enter;
+var charToUse;
 
+//lowercase characters
+  charLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function getRandomChar(arr){
-  var randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
-}
+//uppdercase characters
+  charUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//Numeric characters
+  charNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,];
+
+//Special Characters
+  charSpecial = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]; 
+
+  var getRandomChar;
+
 
 function generatePassword(){
   
-  var charSets = {
-    lowercase: 'abcdefghijklmnopqrstuvwxyz',
-    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    numeric: '0123456789',
-    special: '!@#$%^&*()~`:;\'",<.>/?' 
-  };
+  var getRandomChar;
 
-var charCount = parseInt(window.prompt("How many characters would you like in your password? Choose between 8 and 128"))
-  if (isNaN(charCount)) {
-    window.alert("Please enter a number between 8 and 128")
-     }
-     if (charCount < 8 || charCount > 128){
+charCount = parseInt(window.prompt("How many characters would you like in your password? Please choose between 8 and 128"))
+  
+if (charCount < 8 || charCount > 128){
   window.alert("Has to be between 8 and 128 characters")
   }
 
+  // asks the user if they want the following; Lowercase, Uppdercase, Number, or special.
+  else {
+    
+    charLowercase = window.confirm("Do you want to use Lowercase characters?");
+    charUppercase = window.confirm("Do you want to use Uppercase characters?");
+    charNumber = window.confirm("Do you want to use Numbers?");
+    charSpecial = window.confirm("Do you want to use Special characters?");
 
-var useLowercase = window.confirm("Should your password contain lowercase characters?")
-if (useLowercase){
-  password += getRandomChar(charSets.lowercase);
-  charToUse.push (...charSets.lowercase)
+  };
+
+ if (!charLowercase && !charNumber && !charSpecial && !charUppercase) {
+      charCount = window.alert("You must choose at least one")
+      }
+
+else if (charLowercase && charUppercase && charNumber && charSpecial) {
+  getRandomChar = (charLowercase, charUppercase, charNumber, charSpecial)
 }
 
-var useUppercase = window.confirm("Do you want your password contain uppercase characters?")
-if (useUppercase){
-  password += getRandomChar(charSets.uppercase);
-  charToUse.push (...charSets.uppercase);
-}
+// var password = "";
+// var useLowercase = window.confirm("Should your password contain lowercase characters?")
+// if (useLowercase){
+//   password += getRandomChar(useLowercase);
+//   charToUse.push (useLowercase)
+// }
 
-var useNumeric = window.confirm("Do you want your password to contain numbers?")
-if (useNumeric){
-  password += getRandomChar(charSets.numeric);
-  charToUse.push (...charSets.numeric)
-}
+// var useUppercase = window.confirm("Do you want your password contain uppercase characters?")
+// if (useUppercase){
+//   password += getRandomChar(useUppercase);
+//   charToUse.push (useUppercase);
+// }
 
-var useSpecial = window.confirm("Do you want your password to contain special characters?")
-if(useSpecial){
-  window.confirm = false
-}
-else if (useSpecial){ 
-  password += getRandomChar(charSets.special);
-  charToUse.push (...charSets.special)
-}
+// var useNumber = window.confirm("Do you want your password to contain numbers?")
+// if (useNumber){
+//   password += getRandomChar(useNumber);
+//   charToUse.push (useNumber)
+// }
 
-  while(password.length < charCount){
-    password += getRandomChar(charToUse)
-  }
-if (!useLowercase && !useNumeric && !useSpecial && !useUppercase){
-  choices = alert("You must choose at least one")
+// var useSpecial = window.confirm("Do you want your password to contain special characters?")
+// if (useSpecial){ 
+//   password += getRandomChar(charSets.special);
+//   charToUse.push (...charSets.special)
+// }
+
+  // while(password.length < charCount){
+  //   password += getRandomChar()
+  // }
+}
+  for 
+    var getRandomChar = password = Math.floor(Math.random() * getRandomChar.length);
+    password.push(getRandomChar);
   
-}
   
-}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
